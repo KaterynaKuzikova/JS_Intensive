bb580ea46448058f56a7ad9e3b373e1f8dcdd06d
Kateryna <kuzikova8katena@gmail.com>
2022-08-15 00:45:13 +0300

    feature/1dz_Kuzikova: work with numbers

---
+++ 1dz.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', () => {
+    let firstNumber = + prompt('Enter first value');   
+    let secondNumber = + prompt('Enter second value');
+   
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        alert('Некорректный ввод!');
+        return;
+    }else{
+        alert(firstNumber.toString(secondNumber));
+        return;
+    }
+
+  });
+
+  
+//2подзадание
+
+// window.addEventListener('load', () => {
+//     let firstNumber = + prompt('Enter first value'); 
+
+//     if(isNaN(firstNumber)){
+//         alert('Неккоректный ввод!');
+//         return;
+//     }
+
+//     let secondNumber = + prompt('Inter value B');
+
+//     if(isNaN(secondNumber)){
+//         alert('Неккоректный ввод!');
+//         return;
+//     } else{
+//         let sum = firstNumber+secondNumber;
+//         let quotient = firstNumber/secondNumber;
+//         alert(`Ответ: [ ${sum} ], [ ${quotient} ]`);
+//         return;
+//     }
+
+//   });
